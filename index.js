9bab9f994a3e9905d365233f673f86e991348b6c
Marco Casula <marco.casula@gmail.com>
2022-03-29 10:42:49 +0200

    add index.html

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const processRequest = async (value) => {
   if (!value) {
     throw Error('url is required. Pass it using query string or body')
   }
-  console.dir(value)
   const hash = createHash('sha256').update(value).digest('hex')
   const path = hash.substring(0, 6)
   const publicUrl = `${host}/${path}`.replace(/\/\//g, '/')
@@ -66,7 +65,6 @@ const create = async (req, res) => {
     )
     res.end()
   } catch (error) {
-    console.log(error)
     res.writeHead(200, { 'Content-Type': contentType })
     res.write(JSON.stringify({ error: error.message }))
     res.end()
@@ -76,8 +74,6 @@ const create = async (req, res) => {
 const read = async (req, res) => {
   try {
     const publicUrl = `${req.headers.host}/${req.url}`.replace(/\/\//g, '/')
-
-    console.dir(publicUrl)
     const url = await storage.get(publicUrl)
 
     res.writeHead(301, { Location: url })
@@ -89,12 +85,41 @@ const read = async (req, res) => {
   }
 }
 
+let indexHtml = ''
+
+const loadHtml = () =>
+  new Promise((resolve, reject) => {
+    if (indexHtml) {
+      resolve()
+      return
+    }
+
+    readFile('./index.html', { encoding: 'utf8' }, (err, data) => {
+      if (err) {
+        reject(err)
+      }
+      indexHtml = data
+      resolve()
+    })
+  })
+
+const render = async (req, res) => {
+  await loadHtml()
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write(indexHtml)
+  res.end()
+}
+
 const router = async (req, res) => {
   if (req.method === 'POST') {
     await create(req, res)
     return
   } else if (req.method === 'GET') {
-    await read(req, res)
+    if (req.url.length === 7 && req.url.startsWith('/')) {
+      await read(req, res)
+    } else {
+      await render(req, res)
+    }
     return
   } else {
     res.writeHead(405, { 'Content-Type': contentType })
@@ -117,4 +142,4 @@ void (async () => {
   server.listen(port)
   console.log(`Server running at http://localhost:${port}/, exposed at http://${host}/`)
 })()
-host
+
